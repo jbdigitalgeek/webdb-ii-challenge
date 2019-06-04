05134583c1a24e99877e903bfa7891928646c22d
Jeremy Brady <rhspunkerkid05@yahoo.com>
2019-06-04 13:45:12 -0700

    refactor to index.js, removal of unneeded files

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 const express = require('express');
 const helmet = require('helmet');
-
+const knex = require('knex');
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 
+const knexConfig = {
+    client: 'sqlite3',
+    connection: {
+        filename: './data/lambda.db3',
+    },
+    useNullAsDefault: true,
+};
+
+
+
+
+const db = knex(knexConfig);
+
+
 // endpoints here
+server.get('/', (req, res) => {
+  db('zoos')
+    .then(zoo => {
+      res.status(200).json(zoo);
+    })
+    .catch(error => {
+    res.status(500).json({ message: `${error}`})
+  })
+})
 
 const port = 3300;
 server.listen(port, function() {
