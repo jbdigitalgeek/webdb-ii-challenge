8b7e55d5eee1a783af446b281b25df79be6a6f89
Jeremy Brady <rhspunkerkid05@yahoo.com>
2019-06-04 12:44:08 -0700

    dependencies and knex wired up

---
+++ data/dbConfig.js
@@ -0,0 +1,14 @@
+const knex = require('knex');
+
+const knexConfig = {
+    client: 'sqlite3',
+    connection: {
+        filename: './data/lambda.db3',
+    },
+    useNullAsDefault: true,
+};
+
+const db = knex(knexConfig);
+
+
+module.exports = knex(knexConfig.development);
