8505aa756afa31cf5e6fc67c67a77490419aa470
Jeremy Brady <rhspunkerkid05@yahoo.com>
2019-06-04 13:59:34 -0700

    get's working

--- index.js
+++ index.js
@@ -1,40 +1,46 @@
-const express = require('express');
-const helmet = require('helmet');
-const knex = require('knex');
+const express = require("express");
+const helmet = require("helmet");
+const knex = require("knex");
 const server = express();
-const Zoos = require('./data/data-model')
+const Zoos = require("./data/data-model");
 
 server.use(express.json());
 server.use(helmet());
 
 const knexConfig = {
-    client: 'sqlite3',
-    connection: {
-        filename: './data/lambda.db3',
-    },
-    useNullAsDefault: true,
+  client: "sqlite3",
+  connection: {
+    filename: "./data/lambda.db3"
+  },
+  useNullAsDefault: true
 };
-
-
-
 
 const db = knex(knexConfig);
 
-
 // endpoints here
-server.get('/', (req, res) => {
+server.get("/", (req, res) => {
   Zoos.find()
     .then(zoo => {
       res.status(200).json(zoo);
     })
     .catch(error => {
-      res.status(500).json({ message: `${error}` })
-    })
+      res.status(500).json({ message: `${error}` });
+    });
 });
 
-server.get('/:id', (req, res) => {
-  
-})
+server.get("/:id", (req, res) => {
+  Zoos.findById(req.params.id)
+    .then(animal => {
+      if (animal) {
+        res.status(200).json(animal);
+      } else {
+        res.status(404).json({ message: "Animal not found" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ error: `${error}` });
+    });
+});
 
 const port = 3300;
 server.listen(port, function() {
